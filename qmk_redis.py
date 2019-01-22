348e92570ff5bd2d92fe19c27376693703433a71
skullY <skullydazed@gmail.com>
2019-01-21 22:23:05 -0800

    Add an rq enqueue function

--- qmk_redis.py
+++ qmk_redis.py
@@ -2,12 +2,21 @@ import json
 from os import environ
 
 from redis import Redis
+from rq import Queue
 
 # Configuration
 REDIS_HOST = environ.get('REDIS_HOST', 'redis.qmk-api')
+REDIS_TIMEOUT = int(environ.get('REDIS_TIMEOUT', 180))
 
 # Objects we need to instaniate
 redis = Redis(REDIS_HOST)
+rq = Queue(connection=redis)
+
+
+def enqueue(func, timeout=REDIS_TIMEOUT, *args, **kwargs):
+    """Insert a job into RQ.
+    """
+    return rq.enqueue(func, timeout=timeout, *args, **kwargs)
 
 
 def get(key):
